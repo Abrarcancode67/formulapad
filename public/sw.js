a032de2da6cc3a95c938213f3454b606594c2206
Abrarcancode67 <abrarsobhan2006@gmail.com>
2026-09-02 18:28:01 +0600

    Fix: Update service worker to properly cache manifest and icon

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,25 @@
 const CACHE_NAME = 'formulapad-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
 
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/index.html');
+    })
   );
 });
